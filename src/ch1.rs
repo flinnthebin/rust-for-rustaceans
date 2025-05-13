b38f385b6500c85cb1b43ccff54886e5083c0dac
chris flinn <christopher.flinn@gmail.com>
2025-05-14 09:54:34 +1000

    update

--- src/ch1.rs
+++ src/ch1.rs
@@ -23,5 +23,20 @@ fn copy_move() {
         let z = (x1, y1); // copy x1, move y1
     }
     let x2 = x1; // bind x2 (i32 implements the Copy trait)
-    // let y2 = y1; // illegal (Box doesn't implement copy, so y1 was moved into z and dropped
+    // let y2 = y1; // illegal (Box doesn't implement copy, so y1 was moved into z and dropped)
+}
+
+fn cache(input: &i32, sum: &mut i32) {
+    *sum = *input + *input; // shared refs (&T) are immutable copies
+    assert_eq!(*sum, 2 * *input); // mutable refs (&mut T) are exclusive, only 1 can exist
+}
+
+#[rustfmt::skip]
+fn noalias(input: &i32, output: &mut i32) {
+    if *input == 1 { // shared ref (immutable copy)
+        *output = 2; // mutable ref (exclusive access to target value) collapses to:
+    }                // if input == 1 {
+    if *input != 1 { //     *output = 2;
+        *output = 3; // else {
+    }                //     * output = 3;
 }
